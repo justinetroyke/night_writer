7d8f11f6b25a9478c1b47551899c93e7a6200146
justinetroyke <jjtroyke@gmail.com>
2018-03-24 15:01:29 -0600

    trying to print input 3 times

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,6 +1,16 @@
-
-
 class NightWriter
-  
+
+  attr_reader :input
+
+  def initialize(input)
+    @input = input
+  end
+
+  def output
+    out_3 = 3.times do
+      puts @input
+    end
+      return out_3
+  end
 
 end
