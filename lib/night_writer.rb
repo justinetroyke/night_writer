1bd6591788d63ee4f27678981ab2b0a2e80011c9
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-03-27 18:43:10 -0600

    Pushing to pull master

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,25 +1,14 @@
 require './lib/translator'
 require 'pry'
 
+file_text = File.open(ARGV[0], 'r')
+incoming_english = file_text.read.delete("\n")
+file_text.close
 
-  file_text = File.open(ARGV[0], 'r')
+message = Translator.new(incoming_english)
 
-  # to_translate = File.read(file_text)
-  incoming_english = file_text.read.delete("\n")
-  file_text.close
+writer = File.open(ARGV[1], 'w')
+writer.write(message.braille_result)
+writer.close
 
-  message = Translator.new(incoming_english)
-  message.split_message
-  message.lower_case
-  message.convert_to_braille
-  message.braille_result  # message.top
-  # message.mid
-  # message.bottom
-
-  writer = File.open(ARGV[1], 'w')
-
-  writer.write(message.braille_result)
-
-  writer.close
-
-   # puts "Created '#{ARGV[1]}' containing #{to_translate.strip.length} characters"
+   #puts "Created '#{ARGV[1]}' containing #{to_translate.strip.length} characters"
