df00c496e36dd5e3953a95c5661b7470ca6dcfb0
justinetroyke <jjtroyke@gmail.com>
2018-03-28 19:41:43 -0600

    messed up the writer

--- lib/writer.rb
+++ lib/writer.rb
@@ -1,23 +1,28 @@
 class Writer
 
-  attr_reader :braille_message
+  attr_reader :braille_message,
+              :line1,
+              :line2,
+              :line3
 
   def initialize(braille_message)
     @braille_message = braille_message
+    @line1 = ''
+    @line2 = ''
+    @line3 = ''
+  end
+
+  def sort_lines
+    @braille_message.each_slice(3) do |letter|
+      @line1 << letter[0]
+      @line2 << letter[1]
+      @line3 << letter[2]
+    end
   end
 
   def write_braille
-    line1 = ''
-    line2 = ''
-    line3 = ''
-    @braille_message.each_slice(3) do |letter|
-      line1 << letter[0]
-      line2 << letter[1]
-      line3 << letter[2]
-    end
-
     output_braille = ""
-    while line1.length > 0
+    while @line1.length > 0
       output_braille << line1.slice!(0..79) << "\n"
       output_braille << line2.slice!(0..79) << "\n"
       output_braille << line3.slice!(0..79) << "\n"
