f9e82f2199e3ca2250fc8a0199d0dc6bdbf5e174
justinetroyke <jjtroyke@gmail.com>
2018-03-28 18:14:36 -0600

    GOT THE LENGTH MESSAGE, successfully translated the gettysburg address through writer

--- lib/writer.rb
+++ lib/writer.rb
@@ -8,10 +8,20 @@ class Writer
 
   def write_braille
     line1 = ''
+    line2 = ''
+    line3 = ''
     @braille_message.each_slice(3) do |letter|
       line1 << letter[0]
+      line2 << letter[1]
+      line3 << letter[2]
     end
-    line1
-  end
 
+    output_braille = ""
+    while line1.length > 0
+      output_braille << line1.slice!(0..79) << "\n"
+      output_braille << line2.slice!(0..79) << "\n"
+      output_braille << line3.slice!(0..79) << "\n"
+    end
+  output_braille
+  end
 end
